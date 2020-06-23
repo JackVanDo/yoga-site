39da085bf9b3554407138d6ca9de507c994f05fe
Evgeny <jeton4egg@gmail.com>
2020-06-23 22:04:55 +0300

    Добавил первую версию таймера обратного отсчета на сайт

--- js/script.js
+++ js/script.js
@@ -35,4 +35,44 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //Timer 
+
+    let deadLine = '2020-06-24'; // Создаем переменную в которой указываем конечную дату
+
+    function getTimeRemaning(endtime) {   // функция для определания разницы между конечной и текущей датой
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaning(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }    
+    }
+
+    setClock('timer', deadLine);
 });
